c3e56d4b736fcb6dc2bbdaadf6bfae7f04f3e135
dai <kikuchi.dai@gmail.com>
2021-05-19 14:20:00 +0900

    Created category model, serializer and their tests

--- simple_quizzes_backend/question/models.py
+++ simple_quizzes_backend/question/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Category(models.Model):
+    """Category for quizze"""
+    CATEGORY_CHOICES = (
+        ('math', 'Math'),
+        ('riddle', 'Riddle'),
+    )
+    name = models.CharField(max_length=10,
+                            choices=CATEGORY_CHOICES,
+                            default='math')
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
